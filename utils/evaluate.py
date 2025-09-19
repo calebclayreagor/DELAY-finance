ea97324899a3cdc037529f489070a2300f3c8abd
Caleb C Reagor <ccreagor@gmail.com>
2025-09-19 15:50:21 -0400

    DELAY network volatility prediction

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-def get_sensitivity(window: pd.Timedelta,
-                    symbols: pd.Series,
-                    events: pd.DataFrame,
-                    min_events: int = 3,
-                    ) -> pd.DataFrame:
-    recall = pd.DataFrame(index = symbols, columns = symbols)
-    for ix in symbols:
-        starts = pd.Series(events.index[events[ix].astype(bool)])
-        starts.index = starts.values
-        starts = starts.groupby(starts.index.to_period('W')).min()
-        if starts.size >= min_events:
-            events_i = pd.DataFrame(index = starts.values, columns = symbols)
-            for t in starts:
-                events_t = events.loc[t : (t + window), symbols]
-                events_i.loc[t] = (events_t.sum(0) > 0).astype(int)
-            tpr = events_i.mean(0)
-            tpr_rand = events.loc[:, symbols].mean(0)
-            recall.loc[ix] = (tpr - tpr_rand) / (1 - tpr_rand)
-    return recall
+def get_edges_sensitivity(A: pd.DataFrame,
+                          events: pd.DataFrame,
+                          window: pd.Timedelta,
+                          min_events: int = 3
+                          ) -> pd.DataFrame:
+    A_recall = pd.DataFrame(0., index = A.index, columns = A.columns)
+    for ix in A_recall.index:
+        if A.loc[ix].sum() > 0:
+            starts = pd.Series(events.index[events[ix].astype(bool)])
+            starts.index = starts.values
+            starts = starts.groupby(starts.index.to_period('W')).min()
+            if starts.size >= min_events:
+                events_i = pd.DataFrame(index = starts.values, columns = A.columns)
+                for t in starts:
+                    events_t = events.loc[t : (t + window), A.columns]
+                    events_i.loc[t] = (events_t.sum(0) > 0).astype(int)
+                tpr, tpr_rand = events_i.mean(0), events.loc[:, A.columns].mean(0)
+                A_recall.loc[ix] = (tpr - tpr_rand) / (1 - tpr_rand)
+    return A_recall * A
